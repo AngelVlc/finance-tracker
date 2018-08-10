497ce3387c300e65e2be8044a924c7466f373d48
angelvlc <angelotebh@hotmail.com>
2018-08-10 12:18:14 -0400

    Friends

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,9 @@
 //= require twitter/bootstrap
 //= require turbolinks
 //= require_tree .
+
+$(document).on('turbolinks:load', function() {
+  $('#stock-lookup-form').on('ajax:complete', function(event, data, status){
+    $('#results').html(data.responseText)
+  })
+})
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,16 +1,16 @@
 class Stock < ActiveRecord::Base
   
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+  
   def self.new_from_lookup(ticker_symbol)
-    begin
-      looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
-      price = strip_commas(looked_up_stock.l)
-      new(name: looked_up_stock.name, ticker: looked_up_stock.symbol, last_price: price)
-    rescue Exception => e
-      return nil
-    end
+    looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
+    return nil if !looked_up_stock 
+    new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: looked_up_stock.latest_price)
   end
   
-  def self.strip_commas(number)
-    number.gsub(",", "")
-  end
+  def self.find_by_ticker(ticker_symbol)
+    where(ticker: ticker_symbol).first
+  end  
+  
 end
